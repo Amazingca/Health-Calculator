0f2ab6e19a32586977229f3113645328377288e9
cclute97 <chris.clute.1@gmail.com>
2023-10-23 21:11:55 -0600

    Completed All Data Fields

--- HealthCalcMain.py
+++ HealthCalcMain.py
@@ -134,35 +134,35 @@ class MainWindow(QMainWindow):
         self.layout.addLayout(self.vitamin_layout)
         
         self.mineral_layout = QGridLayout()
-        self.mineral_calcium = QPlainTextEdit("Calcium: ")
+        self.mineral_calcium = QPlainTextEdit("Calcium: " + str(self.calc_target_mineral_calcium()) + " mg")
         self.mineral_calcium.setReadOnly(True)
-        self.mineral_chloride = QPlainTextEdit("Chloride: ")
+        self.mineral_chloride = QPlainTextEdit("Chloride: " + str(self.calc_target_mineral_chloride()) + " g")
         self.mineral_chloride.setReadOnly(True)
-        self.mineral_chromium = QPlainTextEdit("Chromium: ")
+        self.mineral_chromium = QPlainTextEdit("Chromium: " + str(self.calc_target_mineral_chromium()) + " mcg")
         self.mineral_chromium.setReadOnly(True)
-        self.mineral_copper = QPlainTextEdit("Copper: ")
+        self.mineral_copper = QPlainTextEdit("Copper: " + str(self.calc_target_mineral_copper()) + " mcg")
         self.mineral_copper.setReadOnly(True)
-        self.mineral_fluoride = QPlainTextEdit("Fluoride: ")
+        self.mineral_fluoride = QPlainTextEdit("Fluoride: " + str(self.calc_target_mineral_fluoride()) + " mg")
         self.mineral_fluoride.setReadOnly(True)
-        self.mineral_iodine = QPlainTextEdit("Iodine: ")
+        self.mineral_iodine = QPlainTextEdit("Iodine: " + str(self.calc_target_mineral_fluoride()) + " mcg")
         self.mineral_iodine.setReadOnly(True)
-        self.mineral_iron = QPlainTextEdit("Iron: ")
+        self.mineral_iron = QPlainTextEdit("Iron: " + str(self.calc_target_mineral_iron()) + " mg")
         self.mineral_iron.setReadOnly(True)
-        self.mineral_magnesium = QPlainTextEdit("Magnesium: ")
+        self.mineral_magnesium = QPlainTextEdit("Magnesium: " + str(self.calc_target_mineral_magnesium()) + " mg")
         self.mineral_magnesium.setReadOnly(True)
-        self.mineral_manganese = QPlainTextEdit("Manganese: ")
+        self.mineral_manganese = QPlainTextEdit("Manganese: " + str(self.calc_target_mineral_manganese()) + " mg")
         self.mineral_manganese.setReadOnly(True)
-        self.mineral_molybdenum = QPlainTextEdit("Molybdenum: ")
+        self.mineral_molybdenum = QPlainTextEdit("Molybdenum: " + str(self.calc_target_mineral_molybdenum()) + " mcg")
         self.mineral_molybdenum.setReadOnly(True)
-        self.mineral_phosphorous = QPlainTextEdit("Phosphorous: ")
+        self.mineral_phosphorous = QPlainTextEdit("Phosphorous: " + str(self.calc_target_mineral_phosphorous()) + " mg")
         self.mineral_phosphorous.setReadOnly(True)
-        self.mineral_potassium = QPlainTextEdit("Potassium: ")
+        self.mineral_potassium = QPlainTextEdit("Potassium: " + str(self.calc_target_mineral_potassium()) + " g")
         self.mineral_potassium.setReadOnly(True)
-        self.mineral_selenium = QPlainTextEdit("Selenium: ")
+        self.mineral_selenium = QPlainTextEdit("Selenium: " + str(self.calc_target_mineral_selenium()) + " mcg")
         self.mineral_selenium.setReadOnly(True)
-        self.mineral_sodium = QPlainTextEdit("Sodium: ")
+        self.mineral_sodium = QPlainTextEdit("Sodium: " + str(self.calc_target_mineral_sodium()) + " mg")
         self.mineral_sodium.setReadOnly(True)
-        self.mineral_zinc = QPlainTextEdit("Zinc: ")
+        self.mineral_zinc = QPlainTextEdit("Zinc: " + str(self.calc_target_mineral_sodium()) + " mg")
         self.mineral_zinc.setReadOnly(True)
         self.mineral_layout.addWidget(self.mineral_calcium, 0, 0)
         self.mineral_layout.addWidget(self.mineral_chloride, 0, 1)
@@ -415,6 +415,190 @@ class MainWindow(QMainWindow):
         else:
             if self.gender == "m": return 900
             else: return 700
+
+    def calc_target_mineral_calcium(self):
+        # Source: https://ods.od.nih.gov/factsheets/Calcium-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 400
+        elif self.age < 9: return 1000
+        elif self.age < 19: return 1300
+        elif self.age < 51: return 1000
+        elif self.age < 70:
+            if self.gender == "m": return 1000
+            else: return 1200
+        else: return 1200
+
+    def calc_target_mineral_chloride(self):
+        # Source: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7009052/ (Table 03)
+        # Returns dose in g
+
+        if self.age < 4: return 1.7
+        elif self.age < 7: return 2.0
+        elif self.age < 11: return 2.6
+        else: return 3.1 
+
+    def calc_target_mineral_chromium(self):
+        # Source: https://ods.od.nih.gov/factsheets/Chromium-HealthProfessional/
+        # Returns dose in mcg
+
+        if self.age < 4: return 11
+        elif self.age < 9: return 15
+        elif self.age < 14:
+            if self.gender == "m": return 25
+            else: return 21
+        elif self.age < 19:
+            if self.gender == "m": return 35
+            else: return 24
+        elif self.age < 51:
+            if self.gender == "m": return 35
+            else: return 25
+        else: 
+            if self.gender == "m": return 30
+            else: return 20
+
+    def calc_target_mineral_copper(self):
+        # Source: https://ods.od.nih.gov/factsheets/Copper-HealthProfessional/
+        # Returns dose in mcg
+
+        if self.age < 4: return 340
+        elif self.age < 9: return 440
+        elif self.age < 14: return 700
+        elif self.age < 19: return 890
+        else: return 900
+
+    def calc_target_mineral_fluoride(self):
+        # Source: https://ods.od.nih.gov/factsheets/Fluoride-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 0.7
+        elif self.age < 9: return 1
+        elif self.age < 14: return 2
+        elif self.age < 19: return 3
+        else: 
+            if self.gender == "m":
+                return 4
+            else:
+                return 3
+
+    def calc_target_mineral_iodine(self):
+        # Source: https://ods.od.nih.gov/factsheets/Iodine-HealthProfessional/
+        # Returns dose in mcg
+
+        if self.age < 9: return 90
+        elif self.age < 14: return 120
+        else: return 150
+
+    def calc_target_mineral_iron(self):
+        # Source: https://ods.od.nih.gov/factsheets/Iron-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 7
+        elif self.age < 9: return 10
+        elif self.age < 14:
+            if self.gender == "m": return 11
+            else: return 15
+        elif self.age < 51:
+            if self.gender == "m": return 8
+            else: return 18
+        else: return 8
+    
+    def calc_target_mineral_magnesium(self):
+        # Source: https://ods.od.nih.gov/factsheets/Magnesium-HealthProfessional/
+        # Returns value in mg
+
+        if self.age < 4: return 80
+        elif self.age < 9: return 130
+        elif self.age < 14: return 240
+        elif self.age < 19: 
+            if self.gender == "m": return 410
+            else: return 360
+        elif self.age < 31: 
+            if self.gender == "m": return 400
+            else: return 310
+        else:
+            if self.gender == "m": return 420
+            else: return 320
+
+    def calc_target_mineral_manganese(self):
+        # Source: https://ods.od.nih.gov/factsheets/Manganese-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 1.2
+        elif self.age < 9: return 1.5
+        elif self.age < 14: 
+            if self.gender == "m": return 1.9
+            else: return 1.6
+        elif self.age < 19: 
+            if self.gender == "m": return 2.2
+            else: return 1.6
+        else: 
+            if self.gender == "m": return 2.3
+            else: return 1.8
+
+    def calc_target_mineral_molybdenum(self):
+        # Source: https://ods.od.nih.gov/factsheets/Molybdenum-HealthProfessional/
+        # Returns dose in mcg
+
+        if self.age < 4: return 17
+        elif self.age < 9: return 22
+        elif self.age < 14: return 34
+        elif self.age < 19: return 43
+        else: return 45 
+
+    def calc_target_mineral_phosphorous(self):
+        # Source: https://ods.od.nih.gov/factsheets/Phosphorus-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 460
+        elif self.age < 9: return 500
+        elif self.age < 19: return 1250
+        else: return 700 
+
+    def calc_target_mineral_potassium(self):
+        # Source: https://ods.od.nih.gov/factsheets/Potassium-HealthProfessional/
+        # Returns dose in g
+
+        if self.age < 4: return 2
+        elif self.age < 9: return 2.3
+        elif self.age < 14: 
+            if self.gender == "m": return 2.5
+            else: return 2.3
+        elif self.age < 19: 
+            if self.gender == "m": return 3
+            else: return 2.3
+        else: 
+            if self.gender == "m": return 3.4
+            else: return 2.6
+
+    def calc_target_mineral_selenium(self):
+        # Source: https://ods.od.nih.gov/factsheets/Selenium-HealthProfessional/
+        # Returns dose in mcg
+
+        if self.age < 4: return 20
+        elif self.age < 9: return 30
+        elif self.age < 14: return 40
+        else: return 55
+
+    def calc_target_mineral_sodium(self):
+        # Source: fda.gov
+        # Returns dose in mg
+
+        return 2300
+    
+    def calc_target_mineral_zinc(self):
+        # Source: https://ods.od.nih.gov/factsheets/Zinc-HealthProfessional/
+        # Returns dose in mg
+
+        if self.age < 4: return 3
+        elif self.age < 9: return 5
+        elif self.age < 14: return 8 
+        elif self.age < 19: 
+            if self.gender == "m": return 11
+            else: return 9
+        else:
+            if self.gender == "m": return 11
+            else: return 8
 
 if __name__ == "__main__":
 
