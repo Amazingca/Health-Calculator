49431391e53715411d99a251f1a7275ec003cdbd
cclute97 <chris.clute.1@gmail.com>
2023-11-02 14:47:28 -0600

    Fixed Graph X-Axis + Changed MainWindow field colors

--- ProgressGraphWindow.py
+++ ProgressGraphWindow.py
@@ -1,9 +1,8 @@
-import MainWindow, qdarkstyle, sys, csv, datetime
+
+import qdarkstyle, sys, csv, datetime, time
 import pyqtgraph as pg
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QMainWindow
-from pyqtgraph import PlotWidget, plot 
-from matplotlib.dates import date2num
 
 class ProgressGraphWindow(QMainWindow):
     def __init__(self, profile_name, *args, **kwargs):
@@ -11,12 +10,14 @@ class ProgressGraphWindow(QMainWindow):
 
         self.profile_name = profile_name
 
+        pg.setConfigOptions(antialias=True)
+
         # Pull Graph Data from .csv
         self.dates_and_weights = self.get_initial_data() # [[dates], [weights]]
 
         # Set Up Window Title, Size, and Style (Dark)
         self.setWindowTitle("Health History - Health Calculator")
-        self.setFixedSize(QSize(1000, 800))
+        self.setFixedSize(QSize(800, 600))
         self.setStyleSheet(qdarkstyle.load_stylesheet())
 
         # Set up Graph
@@ -27,6 +28,8 @@ class ProgressGraphWindow(QMainWindow):
         self.graphWidget.setLabel('left', 'Weight (lb)')
         self.graphWidget.setLabel('bottom', 'Date')
         self.graphWidget.setTitle('Weight Progress Over Time')
+        self.graphWidget.showGrid(x=True, y=True)
+        
         self.setCentralWidget(self.graphWidget)
 
         self.plot_data()
@@ -43,23 +46,22 @@ class ProgressGraphWindow(QMainWindow):
                     column = 7 # Where data entries begin
                     while column < len(row):
                         current_cell_data = row[column].split(".") # Date.Weight Format
-                        dates_and_weights[0].append(datetime.datetime.strptime(current_cell_data[0], "%m/%d/%Y"))
+                        # The following line converts as follows: String from .csv -> datetime.datetime -> Unix timestamp
+                        dates_and_weights[0].append(time.mktime((datetime.datetime.strptime(current_cell_data[0], "%m/%d/%Y").timetuple())))
                         dates_and_weights[1].append(int(current_cell_data[1]))
                         column += 1
-        
+
         return dates_and_weights
 
     def plot_data(self):
         
-        x = date2num(self.dates_and_weights[0])  # Convert dates to numeric values
+        x = self.dates_and_weights[0] 
         y = self.dates_and_weights[1]
         
-        scatter = pg.ScatterPlotItem(x=x, y=y, size=5, pen=pg.mkPen(None))
-        self.graphWidget.addItem(scatter)
-        self.graphWidget.getAxis("bottom").setStyle(tickTextOffset=20)
-        self.graphWidget.getAxis("bottom").setTextPen("w")
-        self.graphWidget.getAxis("bottom").setPen("w")
-        self.graphWidget.getAxis("bottom").setLabel("Date", color=(255, 255, 255))
+        plot_item = pg.PlotDataItem(x=x, y=y, size=5, pen=pg.mkPen())
+        plot_item.setSymbol('x') 
+        self.graphWidget.addItem(plot_item)
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
